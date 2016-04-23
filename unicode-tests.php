b8f6a200262e4f20b92fd9b5a457e27d4e14cd94
Ash <admin@molura.com>
2016-04-23 16:34:07 +0700

    Init

---
+++ unicode-tests.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ash (ashbeats@gmail.com)
+ * Date: 23-Apr-16
+ * Time: 4:31 PM
+ */
+
+// வார்த்தைகள்
+/* வார்த்தைகள் */
+/*** வார்த்தைகள் */ # subscript kerning fails for phpDoc comments on v2016.1
+/*** "வார்த்தைகள்" */ # subscript kerning works correctly in phpDocs when surrounded with double-quotes. (Issue exists for Khmer as well. Need to test other languages)
+# "வார்த்தைகள்"
+/* todo - வார்த்தைகள் */
+$tamil ="வார்த்தைகள்";
+
+// "ជំរាបសួរ មិត្តភ"
+/* "ជំរាបសួរ មិត្តភ" */
+/*** ជំរាបសួរ មិត្តភ */ #kerning bug
+/*** "ជំរាបសួរ មិត្តភ" */
+/* todo - "ជំរាបសួរ មិត្តភ" */
+$khmer ="ជំរាបសួរ មិត្តភ";
+
+//     
+/*      */
+/***      */
+/* todo -      */
+
+$klingon = "    ";
+
+
+
+
+$array_ko = ['무시되었습니다', '주어진 입력 파일들로부터 번역할 수 있는 문자열을 추출합니다'];
+$text_ko = '무시되었습니다';
+$text_ja = '出力ファイルを指定します';
+$text_fr = 'la définition po n\'est pas donnée.';
+
+
+
